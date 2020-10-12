f8050916f7f2fb4cccd04205b1bc3f551f74b2e6
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2020-10-12 10:11:49 +0200

    ticketing utilisateur avec matricule

---
+++ api/controllers/UserController.js
@@ -0,0 +1,72 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+var bcrypt = require('bcrypt');
+
+module.exports = {
+    create: function(req, res){
+        User.create(req.allParams(), function userCreated(err, user){
+            if(err){
+                console.log("Erreur:" + err);
+            }
+            else{
+              User.findOne({
+                  matricule: req.param('matricule'),
+                }).exec(function (err, user) {
+                  req.session.authenticated = true;
+                  req.session.User = user;
+
+                  // Redirect to protected area
+                  return res.redirect('/dashboard');
+                });
+            };
+        });
+    },
+
+    login: function(req, res) {
+        var matricule = req.param('matricule');
+        var password = req.param('password');
+  
+        // Lookup the user in the database
+        User.findOne({
+            matricule: matricule,
+        }).exec(function (err, user) {
+  
+            // Account not found
+            if (err || !user) {
+                return res.send('Vérifier votre nom ou prénom ou mot de passe', 500);
+            }
+
+            // Compare the passwords
+            bcrypt.compare(password, user.password, function(err, valid) {
+                if(err || !valid)
+                    return res.send('Mot de passe incorrecte', 500)
+
+                // The user has authenticated successfully, set their session
+                req.session.authenticated = true;
+                req.session.User = user;
+
+                // Redirect to protected area
+                return res.redirect('/dashboard');
+            });
+        });
+    },
+
+    logout: function(req, res) {
+        req.session.destroy(function(err) {
+          if(err){
+            return res.send('Erreur déconnexion', 500);
+          }
+          else{
+            return res.redirect('/');
+          }
+  
+        });
+      },
+
+};
+
